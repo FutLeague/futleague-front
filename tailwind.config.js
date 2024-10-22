9a0ff27bbf1a7e276c8d812cb1ed784dae3eaff7
LucianoFedericoC <carduccilucianof@gmail.com>
2024-10-22 15:36:09 -0300

    feat: :sparkles: Auth principal page added

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        futGreenBackground: "#61a562",
+        futBlack: "#000000",
+      },
+      fontFamily: {
+        'SportsWorld': ['"SportsWorld", sans-serif'],
+      }
+    },
   },
   plugins: [],
 }
